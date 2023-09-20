568310954a296813e435b888be23771b7bb45387
Crystyan Gomes <gomescrystyan@gmail.com>
2023-09-20 13:57:57 -0300

    Portifolio

--- app/components/pages/home/hero-section/index.tsx
+++ app/components/pages/home/hero-section/index.tsx
@@ -46,7 +46,7 @@ const Experiencias = [
 export const HeroSection = () => {
   return (
     <section className="w-full lg:h-[755px] bg-hero-image bg-cover bg-center bg-no-repeat flex flex-col justify-end pb-10 sm:pb-32 py-32 lg:pb-[110px]">
-      <div className=" mt-5 bg-black/60 rounded flex items-start justify-between flex-col-reverse lg:flex-row">
+      <div className="container flex items-start justify-between flex-col-reverse lg:flex-row">
         <motion.div
           initial={{ opacity: 0, x: -100 }}
           whileInView={{ opacity: 1, x: 0 }}
