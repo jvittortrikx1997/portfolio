bf40cf8851abf65f646ac59d62d546291129fffe
João Vitor Mendonça <91424123+jvittortrikx1997@users.noreply.github.com>
2024-12-13 11:25:46 -0300

    fix:coolors

--- app/components/modal/index.tsx
+++ app/components/modal/index.tsx
@@ -10,8 +10,8 @@ export const Modal = ({ isOpen = false, onCLick = () => {} }) => {
       <div className="h-60 absolute w-72 bg-gray-800 border border-sky-400 rounded">
         <div className="w-full h-full flex flex-col items-center justify-center">
           <div className="w-full h-full flex flex-col justify-center items-center">
-            <p className="font-medium text-lg text-sky-600">Giulia</p>
-            <p className="font-medium text-base text-sky-600">Amor</p>
+            <p className="font-medium text-lg text-sky-600">Deus</p>
+            <p className="font-medium text-base text-sky-600">Corinthians</p>
           </div>
           <div className="container w-full h-full flex justify-center items-center">
             <p className="font-medium text-lg text-gray-400">
@@ -22,7 +22,7 @@ export const Modal = ({ isOpen = false, onCLick = () => {} }) => {
           <div className="w-full h-full gap-4 flex justify-center items-center">
             <GiNestedHearts color="#db0505" />
             <p className="font-medium text-lg text-gray-100">
-              Com amor, Crystyan
+              Com amor, João Vitor
             </p>
             <GiNestedHearts color="#db0505" />
           </div>
